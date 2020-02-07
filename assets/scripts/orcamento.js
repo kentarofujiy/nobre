1b6ba50f25d16c9d78a9f7ed4e02a44fdd46e221
kentaro fujiy <kentaro@manacadigital.com.br>
2020-02-07 01:09:06 +0000

    finish

--- assets/scripts/orcamento.js
+++ assets/scripts/orcamento.js
@@ -6,11 +6,11 @@ app.controller("orcamentoCtrl", function ($scope, $http) {
 		console.dir($scope.formData);
 		$http({
 			method: "POST",
-			url: "https://sotreqlab.herokuapp.com/form320cats.json",
+			url: "http://64.225.8.81:8003/nobreforms.json",
 			data: $.param($scope.formData),
 			headers: { "Content-Type": "application/x-www-form-urlencoded"
 			} }).then(function (){
-                window.location = "https://sotreq.com.br/escavadeira320/enviado.html";
+                window.location = "http://nobrefood.com.br/enviado.html";
             });
 		$scope.formData = {};
 		$scope.orcamentoForm.$setPristine();
@@ -20,23 +20,3 @@ app.controller("orcamentoCtrl", function ($scope, $http) {
 
 });
 
-app.controller("orcamentoCtrl2", function ($scope, $http) {
-	$scope.formData = {};
-	$scope.processForm = function () {
-
-		console.dir($scope.formData);
-		$http({
-			method: "POST",
-			url: "https://sotreqlab.herokuapp.com/form320cats.json",
-			data: $.param($scope.formData),
-			headers: { "Content-Type": "application/x-www-form-urlencoded"
-			} }).then(function (){
-                window.location = "https://sotreq.com.br/escavadeira320/enviado2.html";
-            });
-		$scope.formData = {};
-		$scope.orcamentoForm.$setPristine();
-        $scope.orcamentoForm.$setUntouched();
-
-           };
-
-});
