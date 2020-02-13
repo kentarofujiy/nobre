baab69abca1a50abab13f4f74b8f3e6eae30d3ea
kentaro fujiy <kentaro@manacadigital.com.br>
2020-02-13 00:02:20 +0000

    fixes

--- assets/scripts/orcamento.js
+++ assets/scripts/orcamento.js
@@ -6,7 +6,7 @@ app.controller("orcamentoCtrl", function ($scope, $http) {
 		console.dir($scope.formData);
 		$http({
 			method: "POST",
-			url: "http://64.225.8.81:8003/nobreforms.json",
+			url: "http://64.225.8.81:8004/nobreforms.json",
 			data: $.param($scope.formData),
 			headers: { "Content-Type": "application/x-www-form-urlencoded"
 			} }).then(function (){
